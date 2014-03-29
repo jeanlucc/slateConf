12d5ac7653ed9e30cfd363b2bc4237f52ed82847
votre_pseudo <moi@email.com>
2014-03-29 12:06:41 +0100

    Added precision in the test of window position

--- slate.js
+++ slate.js
@@ -26,13 +26,13 @@ function place2Windows()
     }
 
     // Perform appropriate action.
-    if (isWindowPosition(currentWindow, 0, 0, 1, 1) &&
-        isWindowPosition(secondWindow, 0, 0, 1, 1)) {
+    if (isWindowAt(currentWindow, 0, 0, 1, 1) &&
+        isWindowAt(secondWindow, 0, 0, 1, 1)) {
         // Change to split vertical
         moveWindowAt(currentWindow, 0, 0, 1/2, 1);
         moveWindowAt(secondWindow, 1/2, 0, 1/2, 1);
-    } else if (isWindowPosition(currentWindow, 0, 0, 1/2, 1) &&
-               isWindowPosition(secondWindow, 1/2, 0, 1/2, 1)) {
+    } else if (isWindowAt(currentWindow, 0, 0, 1/2, 1) &&
+               isWindowAt(secondWindow, 1/2, 0, 1/2, 1)) {
         // Change to split horizontal
         moveWindowAt(currentWindow, 0, 0, 1, 1/2);
         moveWindowAt(secondWindow, 0, 1/2, 1, 1/2);
@@ -169,9 +169,38 @@ function adjustWindowsSizes()
  * bottom-right corner of the screen. For the size 1 is the size of
  * the screen.
  */
-function isWindowPosition(window, x, y, w, h)
+function isWindowAt(window, x, y, w, h)
 {
-    var pos = new Array(x, y, w, h);
+    var precision = 1;
+    return isWindowAtWithPrecision(
+        window,
+        [x, precision, precision],
+        [y, precision, precision],
+        [w, precision, precision],
+        [h, precision, precision]
+    );
+}
+
+/**
+ * Tests the position of the window passed in parameter with the given
+ * precisions in pixel. The x and y parameters corresponds to the
+ * coordinates of the top-left corner, w to the width and h to the
+ * height of the desired position. Those four arguments are arrays of
+ * three elements the first element is the value, the next is the
+ * tolerated precision for bigger than expected value and the last is
+ * the tolerated precision for smaller than expected value. The values
+ * must be between 0 and 1 or any string which will be interpreted as
+ * a wildcard. The precision must be an integer greater than 1, any
+ * other value will be interpreted as 1. For the coordinates (0, 0) is
+ * the top-left and (1, 1) is the bottom-right corner of the
+ * screen. For the sizes 1 is the size of the screen.
+ */
+function isWindowAtWithPrecision(window, x, y, w, h)
+{
+    var defaultPrecision = 1;
+    var pos = new Array(x[0], y[0], w[0], h[0]);
+    var precisionBigger = new Array(x[1], y[1], w[1], h[1]);
+    var precisionSmaller = new Array(x[2], y[2], w[2], h[2]);
 
     conditions = new Array();
     // Find wildcards
@@ -180,10 +209,16 @@ function isWindowPosition(window, x, y, w, h)
             conditions[i] = true;
         } else if (typeof pos[i] !== 'number' || pos[i] > 1 || pos[i] < 0) {
             return false;
+        } else {
+            if (typeof precisionBigger[i] !== "integer" || precisionBigger[i] < 1) {
+                precisionBigger[i] = defaultPrecision;
+            }
+            if (typeof precisionSmaller[i] !== "integer" || precisionSmaller[i] < 1) {
+                precisionSmaller[i] = defaultPrecision;
+            }
         }
     }
 
-    var precision = 1;
     var screen = window.screen();
     var screenOriginX = screen.visibleRect().x;
     var screenOriginY = screen.visibleRect().y;
@@ -191,16 +226,27 @@ function isWindowPosition(window, x, y, w, h)
     var screenSizeY = screen.visibleRect().height;
 
     if (! conditions[0]) {
-        conditions[0] = Math.abs(window.rect().x - (screenOriginX + pos[0] * screenSizeX)) < precision;
+        conditions[0] =
+            ((window.rect().x - (screenOriginX + pos[0] * screenSizeX)) < precisionBigger[0]) &&
+            (-(window.rect().x - (screenOriginX + pos[0] * screenSizeX)) < precisionSmaller[0]);
     }
+
     if (! conditions[1]) {
-        conditions[1] = Math.abs(window.rect().y - (screenOriginY + pos[1] * screenSizeY)) < precision;
+        conditions[1] =
+            ((window.rect().y - (screenOriginY + pos[1] * screenSizeY)) < precisionBigger[1]) &&
+            (-(window.rect().y - (screenOriginY + pos[1] * screenSizeY)) < precisionSmaller[1]);
     }
+
     if (! conditions[2]) {
-        conditions[2] = Math.abs(window.rect().width - (pos[2] * screenSizeX)) < precision;
+        conditions[2] =
+            ((window.rect().width - (pos[2] * screenSizeX)) < precisionBigger[2]) &&
+            (-(window.rect().width - (pos[2] * screenSizeX)) < precisionSmaller[2]);
     }
+
     if (! conditions[3]) {
-        conditions[3] = Math.abs(window.rect().height - (pos[3] * screenSizeY)) < precision;
+        conditions[3] =
+            ((window.rect().height - (pos[3] * screenSizeY)) < precisionBigger[3]) &&
+            (-(window.rect().height - (pos[3] * screenSizeY)) < precisionSmaller[3]);
     }
 
     return conditions[0] && conditions[1] && conditions[2] && conditions[3];
