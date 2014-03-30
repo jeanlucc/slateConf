b29b9117c838f27f6af8ba67affc588872c736b6
Jean-Luc Colombier <jean.luc.colombier@gmail.com>
2014-03-30 10:35:49 +0200

    Renamed slate.js

--- .slate.js
+++ .slate.js
@@ -145,11 +145,6 @@ slate.bindAll({
     "i:ctrl;cmd"      : swapWindows,
     "o:ctrl;cmd"      : place2Windows,
     "p:ctrl;cmd"      : adjust2WindowsSizes,
-    "a:ctrl;cmd"      : throwToPreviousScreen,
-    "z:ctrl;cmd"      : throwToNextScreen,
-    "1:ctrl;cmd"      : throwToFirstScreen,
-    "2:ctrl;cmd"      : throwToSecondScreen,
-    "3:ctrl;cmd"      : throwToThirdScreen,
 
     // Modal bindings
     // moves
@@ -173,11 +168,6 @@ slate.bindAll({
     "i:m;ctrl;cmd:toggle"      : swapWindows,
     "o:m;ctrl;cmd:toggle"      : place2Windows,
     "p:m;ctrl;cmd:toggle"      : adjust2WindowsSizes,
-    "a:m;ctrl;cmd:toggle"      : throwToPreviousScreen,
-    "z:m;ctrl;cmd:toggle"      : throwToNextScreen,
-    "1:m;ctrl;cmd:toggle"      : throwToFirstScreen,
-    "2:m;ctrl;cmd:toggle"      : throwToSecondScreen,
-    "3:m;ctrl;cmd:toggle"      : throwToThirdScreen,
 });
 
 // End of bindings
@@ -368,6 +358,18 @@ function getScreenRelativeWindowPosition(window)
     return new Array(x, y, w, h);
 }
 
+/**
+ *  Loads a ".js" file by name without ".js" extension in directory
+ *  "~/.slate". Log a message on error.
+ */
+function load(fileName)
+{
+    var path = "~/.slate/" + fileName + ".js";
+    if (! slate.source(path)) {
+        slate.log("Error while loading " + path);
+    }
+}
+
 // End of auxiliary functions
 // __________________________
 
@@ -512,84 +514,5 @@ function adjust2WindowsSizes()
 // End of advanced functions
 // _________________________
 
-// Throw functions
-// _______________
-
-/**
- * Throws the window to next screen keeping the same position
- * relatively to screen.
- */
-function throwToNextScreen()
-{
-    // Get next screen
-    var nextId = (window.screen().id() + 1) % slate.screenCount();
-    var screen = slate.screenForRef(nextId);
-
-    throwToScreen(screen);
-}
-
-/**
- * Throws the window to previous screen keeping the same position
- * relatively to screen.
- */
-function throwToPreviousScreen()
-{
-    // Get previous screen
-    var previousId = (window.screen().id() - 1) % slate.screenCount();
-    var screen = slate.screenForRef(nextId);
-
-    throwToScreen(screen);
-}
-
-/**
- * Throws the window to first screen keeping the same position
- * relatively to screen.
- */
-function throwToFirstScreen()
-{
-    // Get first screen
-    var screen = slate.screenForRef("0");
-
-    throwToScreen(screen);
-}
-
-/**
- * Throws the window to second screen keeping the same position
- * relatively to screen.
- */
-function throwToSecondScreen()
-{
-    // Get second screen
-    var screen = slate.screenForRef("1");
-
-    throwToScreen(screen);
-}
-
-/**
- * Throws the window to third screen keeping the same position
- * relatively to screen.
- */
-function throwToThirdScreen()
-{
-    // Get third screen
-    var screen = slate.screenForRef("2");
-
-    throwToScreen(screen);
-}
-
-/**
- * Throws the window to the given screen keeping the same position
- * relatively to screen.
- */
-function throwToScreen(screen)
-{
-    // Get current window
-    var window = slate.window();
-    // Get window position relatively to the screen
-    var pos = getScreenRelativeWindowPosition(window);
-
-    moveWindowToScreenAt(window, pos[0], pos[1], pos[2], pos[3], screen);
-}
-
-// End throw functions
-// ___________________
+// Throw
+load("throw");
